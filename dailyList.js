e42506ad712a4b86cf629cb02903f20f9bf06785
YM.devProg <muhammadyusuf.yusupov201@gmail.com>
2025-06-24 11:51:00 +0500

    Delete dailyList.js

--- dailyList.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('Continue').addEventListener('click', function () {
-    const name = document.getElementById('InputName').value.trim();
-    if (name == "") {
-        alert("Please enter your name!");
-        return;
-    }
-    window.location.href = "dailyList-2.html";
-});
